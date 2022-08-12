1f809d48ea2632135708a3f58f20288c81f825fb
hclbaur <hclbaur@gmail.com>
2022-08-12 14:01:06 +0200

    Refactored demo.

--- src/test/java/demo.java
+++ src/test/java/demo.java
@@ -1,10 +1,8 @@
 import java.io.FileReader;
 
-import be.baur.sda.ComplexNode;
 import be.baur.sda.Node;
 import be.baur.sda.NodeSet;
 import be.baur.sda.SDA;
-import be.baur.sda.SimpleNode;
 import be.baur.sds.SDS;
 import be.baur.sds.Schema;
 import be.baur.sds.validation.Error;
@@ -18,23 +16,22 @@ public class demo {
 		Schema schema = SDS.parser().parse(sds);
 		
 		FileReader sda = new FileReader(args[1]);
-		ComplexNode root = (ComplexNode) SDA.parser().parse(sda);
+		Node root = SDA.parser().parse(sda);
 		
 		ErrorList errors = SDS.validator().validate(root, schema, null);
 		if (! errors.isEmpty()) {
 			for (Error error : errors) System.out.println(error.toString()); return;
 		}
 		
-		for (Node c : root.getNodes().get("contact")) {
+		for (Node contact : root.getNodes().get("contact")) {
 			
-			ComplexNode contact = (ComplexNode) c;
-			SimpleNode name = (SimpleNode) contact.getNodes().get("firstname").get(1);
+			Node name = contact.getNodes().get("firstname").get(1);
 			NodeSet numbers = contact.getNodes().get("phonenumber");
 			
 			System.out.println(name.getValue() + " has " + numbers.size() + " phone number(s).");
 			
-			int i = 0; 	for (Node n : numbers) {
-				System.out.println("  Number " + ++i + ": " + ((SimpleNode) n).getValue());
+			int i = 0; 	for (Node number : numbers) {
+				System.out.println("  Number " + ++i + ": " + number.getValue());
 			}
 		}
 	}
